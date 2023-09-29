8e11fd08ea1262902038b720d26ed0ee24a5ab0b
Ole Jacob Mellgren <olejme@stud.ntnu.no>
2023-09-29 13:32:42 +0200

    #1 Setup router to pages
    Setup router to pages and made template for different pages

---
+++ src/pages/Favoritepage.tsx
@@ -0,0 +1,11 @@
+
+const Favoritepage = () => {
+  return (
+    <div>
+      <h1>Hello from favoritepage</h1>
+      <p>This is a basic favoritepage component.</p>
+    </div>
+  );
+};
+
+export default Favoritepage;
---
+++ src/pages/Filterpage.tsx
@@ -0,0 +1,11 @@
+
+const Filterpage = () => {
+  return (
+    <div>
+      <h1>Hello from Filterpage</h1>
+      <p>This is a basic filterpage component.</p>
+    </div>
+  );
+};
+
+export default Filterpage;
