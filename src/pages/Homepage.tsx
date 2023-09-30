d9430a90d069cdc4c313e2364edc372e692c1bae
andretan <andretan@stud.ntnu.no>
2023-10-01 00:04:10 +0200

    #3 improved styling of dropdownMenu component
    - Now multiple dropdownMenu's can be used

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -2,8 +2,10 @@ import { Link } from 'react-router-dom';
 import DropdownMenu from '../components/DropdownMenu';
 
 const Homepage = () => {
-  const filter = "Year"
-  const options = ["2018", "2017", "2016", "2015", "Alle"]
+  const filter1 = "Model"
+  const options1 = ["Volvo", "BMW", "Volkswagen", "Mercedes", "Alle"]
+  const filter2 = "Year"
+  const options2 = ["2018", "2017", "2016", "2015", "Alle"]
 
   return (
     <div>
@@ -12,7 +14,8 @@ const Homepage = () => {
       <button>
         <Link to="/project2/carpage/123">Go to Carpage</Link>
       </button>
-      <DropdownMenu filter={filter} options={options} />
+      <DropdownMenu filter={filter1} options={options1} />
+      <DropdownMenu filter={filter2} options={options2} />
     </div>
   );
 };
