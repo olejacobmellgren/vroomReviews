48732886e465a02f18ccfe4d71c4c57f9cc13719
Ole Jacob Mellgren <olejme@stud.ntnu.no>
2023-10-01 18:23:23 +0200

    #4 last prettier run

--- src/__tests__/Header.test.tsx
+++ src/__tests__/Header.test.tsx
@@ -1,27 +1,27 @@
-import { render } from "@testing-library/react";
-import Header from "../components/Header";
-import { test, expect } from "vitest";
-import userEvent from "@testing-library/user-event";
-import { BrowserRouter } from "react-router-dom";
+import { render } from '@testing-library/react';
+import Header from '../components/Header';
+import { test, expect } from 'vitest';
+import userEvent from '@testing-library/user-event';
+import { BrowserRouter } from 'react-router-dom';
 
-test("Go to different pages", async () => {
+test('Go to different pages', async () => {
   const { getByText, asFragment } = render(
     <BrowserRouter>
       <Header />
-    </BrowserRouter>
+    </BrowserRouter>,
   );
 
-  const browse = getByText("Browse cars");
+  const browse = getByText('Browse cars');
   expect(browse).toBeTruthy(); // The text is found
   await userEvent.click(browse);
   expect(asFragment()).toMatchSnapshot();
 
-  const favorites = getByText("Favorites");
+  const favorites = getByText('Favorites');
   expect(favorites).toBeTruthy(); // The text is found
   await userEvent.click(favorites);
   expect(asFragment()).toMatchSnapshot();
 
-  const reviews = getByText("Your reviews");
+  const reviews = getByText('Your reviews');
   expect(reviews).toBeTruthy(); // The text is found
   await userEvent.click(reviews);
   expect(asFragment()).toMatchSnapshot();
