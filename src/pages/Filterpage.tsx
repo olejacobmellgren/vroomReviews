9829e89fa2cc17a812fa79aed3f3c9de8719523b
andretan <andretan@stud.ntnu.no>
2023-10-07 19:17:15 +0200

    #5 ran prettier

--- src/pages/Filterpage.tsx
+++ src/pages/Filterpage.tsx
@@ -1,8 +1,8 @@
 import DropdownMenu from '../components/DropdownMenu';
 import { useState } from 'react';
-import '../assets/FilterPage.css'
+import '../assets/FilterPage.css';
 import CardForCar from '../components/CardForCar';
-import cars from '../cars/cars.json'
+import cars from '../cars/cars.json';
 
 type CarInfo = {
   id: number;
@@ -25,46 +25,51 @@ type CarInfo = {
 
 const Filterpage = () => {
   const filters = [
-    { name: 'Brand', options: ['Ferrari', 'Hyundai', 'Toyota', 'BMW', 'Audi', 'Volvo', 'All'] },
+    {
+      name: 'Brand',
+      options: ['Ferrari', 'Hyundai', 'Toyota', 'BMW', 'Audi', 'Volvo', 'All'],
+    },
     { name: 'Year', options: ['2023', '2022', '2021', '2020', '2019', 'All'] },
     { name: 'Body', options: ['Coupe', 'SUV', 'Sedan', 'All'] },
-    { name: 'Sort by', options: ['Reviews', 'Rating', 'All'] }
+    { name: 'Sort by', options: ['Reviews', 'Rating', 'All'] },
   ];
 
   const [selectedFilters, setSelectedFilters] = useState({
     Brand: 'All',
     Year: 'All',
     Body: 'All',
-    SortBy: 'All'
+    SortBy: 'All',
   });
 
   // initialize each dropdownMenu to be false, meaning options are not shown
   const [dropdownVisibility, setDropdownVisibility] = useState(
-    filters.map(() => false)
+    filters.map(() => false),
   );
 
   const [visibleCars, setVisibleCars] = useState(12);
 
   const handleViewMore = () => {
-    setVisibleCars(prevVisibleCars => prevVisibleCars + 12);
+    setVisibleCars((prevVisibleCars) => prevVisibleCars + 12);
   };
 
   const handleFilterChange = (filterName: string, selectedValue: string) => {
-    setSelectedFilters(prevSelectedFilters => ({
+    setSelectedFilters((prevSelectedFilters) => ({
       ...prevSelectedFilters,
-      [filterName]: selectedValue
+      [filterName]: selectedValue,
     }));
   };
 
   // display dropdown for dropdownMenu clicked. The rest is set to false, meaning they are closed.
   // This ensures that only 1 dropdown can be open at a time.
   const toggleDropdown = (index: number) => {
-    setDropdownVisibility(dropdownVisibility.map((item, i) => i === index ? !item : false));
+    setDropdownVisibility(
+      dropdownVisibility.map((item, i) => (i === index ? !item : false)),
+    );
   };
 
   const applyFilters = (cars: CarInfo[]) => {
-    return cars.filter(car => {
-      return filters.every(filter => {
+    return cars.filter((car) => {
+      return filters.every((filter) => {
         if (filter.name === 'Brand' && selectedFilters.Brand !== 'All') {
           return car.brand === selectedFilters.Brand;
         }
@@ -95,17 +100,19 @@ const Filterpage = () => {
         ))}
       </div>
       <div className="carList">
-        {applyFilters(cars).slice(0, visibleCars).map((car) => (
-          <div className="car" key={car.id}>
-            <CardForCar
-              id={car.id.toString()}
-              brand={car.brand}
-              model={car.model}
-              carIMG={car.image}
-              showInfo={true}
-            />
-          </div>
-        ))}
+        {applyFilters(cars)
+          .slice(0, visibleCars)
+          .map((car) => (
+            <div className="car" key={car.id}>
+              <CardForCar
+                id={car.id.toString()}
+                brand={car.brand}
+                model={car.model}
+                carIMG={car.image}
+                showInfo={true}
+              />
+            </div>
+          ))}
       </div>
       <div className="view-more-button">
         {visibleCars < applyFilters(cars).length ? (
