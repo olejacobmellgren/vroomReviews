13d4cd0a95142aa09ed5be27595b58a68bd737e2
Ole Jacob Mellgren <olejme@stud.ntnu.no>
2023-11-08 20:58:19 +0100

    #43 fix last setup stuff

--- frontend/src/__tests__/Header.test.tsx
+++ frontend/src/__tests__/Header.test.tsx
@@ -5,23 +5,26 @@ import userEvent from '@testing-library/user-event';
 import { BrowserRouter } from 'react-router-dom';
 
 test('Go to different pages', async () => {
+  function setPage(page: string) {
+    return page;
+  }
   const { getByText, asFragment } = render(
     <BrowserRouter>
-      <Header />
+      <Header page={"home"} setPage={setPage}/>
     </BrowserRouter>,
   );
 
-  const browse = getByText('Browse cars');
+  const browse = getByText('Search');
   expect(browse).toBeTruthy(); // The text is found
   await userEvent.click(browse);
   expect(asFragment()).toMatchSnapshot();
 
-  const favorites = getByText('Favorites');
+  const favorites = getByText('My Favorites');
   expect(favorites).toBeTruthy(); // The text is found
   await userEvent.click(favorites);
   expect(asFragment()).toMatchSnapshot();
 
-  const reviews = getByText('Your reviews');
+  const reviews = getByText('My Reviews');
   expect(reviews).toBeTruthy(); // The text is found
   await userEvent.click(reviews);
   expect(asFragment()).toMatchSnapshot();
