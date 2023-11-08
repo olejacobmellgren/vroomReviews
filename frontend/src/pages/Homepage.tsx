1cb3aa40a9bf5adcffe3a0156e33f53a0336b4e8
Filip Skaug <filis@stud.ntnu.no>
2023-11-08 04:42:01 +0100

    #36 run prettier and lint

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -10,7 +10,7 @@ interface company {
 }
 
 const Homepage = () => {
-  const [visibleBrands, setVisibleBrands] = useState([0,3]);
+  const [visibleBrands, setVisibleBrands] = useState([0, 3]);
   // Get all cars that are favorited by user
   const { loading, error, data } = useQuery(GET_COMPANIES, {});
   if (loading) return <CircularProgress />;
@@ -19,35 +19,44 @@ const Homepage = () => {
   const increment = 3;
   const viewNext = () => {
     if (data.companies.length - visibleBrands[1] > increment) {
-      setVisibleBrands([visibleBrands[0] + increment, visibleBrands[1] + increment]);
-    }
-    else {
-      setVisibleBrands([data.companies.length - increment, data.companies.length]);
+      setVisibleBrands([
+        visibleBrands[0] + increment,
+        visibleBrands[1] + increment,
+      ]);
+    } else {
+      setVisibleBrands([
+        data.companies.length - increment,
+        data.companies.length,
+      ]);
     }
   };
   const viewPrev = () => {
     if (visibleBrands[0] > increment) {
-      setVisibleBrands([visibleBrands[0] - increment, visibleBrands[1] - increment]);
-    }
-    else {
-      setVisibleBrands([0,increment]);
+      setVisibleBrands([
+        visibleBrands[0] - increment,
+        visibleBrands[1] - increment,
+      ]);
+    } else {
+      setVisibleBrands([0, increment]);
     }
   };
 
   return (
     <>
-      {data.companies.slice(visibleBrands[0], visibleBrands[1]).map((data: company) => (
-        <div className="conteiner">
-          <div className="scrollingMenuHeader">
-            <div className="element"></div>
-            <h1>{data.name}</h1>
-            <div className="brand-logo-wrapper">
-              <img className="brand-logo" src={data.logo} alt="noLogo" />
+      {data.companies
+        .slice(visibleBrands[0], visibleBrands[1])
+        .map((data: company) => (
+          <div className="conteiner">
+            <div className="scrollingMenuHeader">
+              <div className="element"></div>
+              <h1>{data.name}</h1>
+              <div className="brand-logo-wrapper">
+                <img className="brand-logo" src={data.logo} alt="noLogo" />
+              </div>
             </div>
+            <ScrollingMenu brand={data.name} />
           </div>
-          <ScrollingMenu brand={data.name} />
-        </div>
-      ))}
+        ))}
       <div className="view-more-button">
         <button onClick={viewPrev}>Prev Page</button>
         <button onClick={viewNext}>Next Page</button>
