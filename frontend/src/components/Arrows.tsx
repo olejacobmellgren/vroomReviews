a875a409412581b35f2aa74e4337ac0cb3bb5ee3
Filip Skaug <filis@stud.ntnu.no>
2023-11-08 04:25:17 +0100

    design button and scroll by all elements

--- frontend/src/components/Arrows.tsx
+++ frontend/src/components/Arrows.tsx
@@ -1,57 +1,80 @@
 import React from "react";
-
 import { VisibilityContext } from "react-horizontal-scrolling-menu";
 
 function Arrow({
   children,
+  disabled,
   onClick
 }: {
   children: React.ReactNode;
+  disabled: boolean;
   onClick: VoidFunction;
 }) {
   return (
-    <button
-      onClick={onClick}
-      style={{
-        cursor: "pointer",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        right: "1%",
-        userSelect: "none",
-      }}
-    >
-      {children}
-    </button>
+    <>
+      <button disabled={disabled} className="arrow-button" onClick={onClick}>
+        {children}
+      </button>
+    </>
   );
 }
 
 export function LeftArrow() {
   const {
-    getPrevElement,
-    scrollToItem,
+    isFirstItemVisible,
+    scrollPrev,
+    visibleElements,
+    initComplete
   } = React.useContext(VisibilityContext);
 
-  // NOTE: for scroll 1 item
-  const clickHandler = () => scrollToItem(getPrevElement(), "smooth", "start");
+  const [disabled, setDisabled] = React.useState(
+    !initComplete || (initComplete && isFirstItemVisible)
+  );
+  React.useEffect(() => {
+    // NOTE: detect if whole component visible
+    if (visibleElements.length) {
+      setDisabled(isFirstItemVisible);
+    }
+  }, [isFirstItemVisible, visibleElements]);
+
   return (
-    <Arrow onClick={clickHandler}>
-      Left
-    </Arrow>
+    <div className="left-arrow-wrapper">
+      <Arrow disabled={disabled} onClick={() => scrollPrev()}>
+        <div className="arrow left">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </Arrow>
+    </div>    
   );
 }
 
 export function RightArrow() {
-  const {
-    getNextElement,
-    scrollToItem,
-  } = React.useContext(VisibilityContext);
+  const { isLastItemVisible, scrollNext, visibleElements } = React.useContext(
+    VisibilityContext
+  );
 
-  // NOTE: for scroll 1 item
-  const clickHandler = () => scrollToItem(getNextElement(), "smooth", "end");
+  // console.log({ isLastItemVisible });
+  const [disabled, setDisabled] = React.useState(
+    !visibleElements.length && isLastItemVisible
+  );
+
+  React.useEffect(() => {
+    if (visibleElements.length) {
+      setDisabled(isLastItemVisible);
+    }
+  }, [isLastItemVisible, visibleElements]);
+
   return (
-    <Arrow onClick={clickHandler}>
-      Right
-    </Arrow>
+    <div className="right-arrow-wrapper">
+      <Arrow disabled={disabled} onClick={() => scrollNext()}>
+        <div className="arrow right">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </Arrow>
+    </div>    
   );
 }
