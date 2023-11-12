0c48a666c891f57f70ed697752d9fccf658b4bb4
Ole Jacob Mellgren <olejme@stud.ntnu.no>
2023-11-12 21:56:31 +0100

    #46 made alertpop component test

---
+++ frontend/src/__tests__/AlertPopup.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import { test, expect } from 'vitest';
+import userEvent from '@testing-library/user-event';
+import AlertPopup from '../components/AlertPopup';
+
+
+describe('AlertPopup Component', () => {
+  test('matches snapshot', () => {
+    const { asFragment } = render(
+      <AlertPopup visible={true} message="Test message" />
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  test('closes alert on close button click', () => {
+    render(<AlertPopup visible={true} message="Test message" />);
+    const closeBtn = screen.getByRole('button', { name: /close/i });
+    userEvent.click(closeBtn);
+    const alert = screen.queryByText('Test message');
+    expect(alert).toBeTruthy();
+  });
+});
