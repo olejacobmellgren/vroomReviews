c8c45d71753e347dfea98184a59b7dc42eebf66e
Filip Skaug <filis@stud.ntnu.no>
2023-11-14 23:52:14 +0100

    #18 add logo to header, and but dropdown on right + styling

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import '../assets/Header.css';
+import logo from '/public/logo.png';
 
 const Header = ({
   page,
@@ -24,60 +25,53 @@ const Header = ({
   }
 
   return (
-    <div className="header-container">
-      <input
-        className="check"
-        checked={isChecked}
-        onChange={toggleCheckbox}
-        type="checkbox"
-        id="check"
-      />
-      <label className="checkbtn" htmlFor="check">
-        <img
-          className="burger-menu"
-          src="https://www.pngkit.com/png/full/239-2394744_icon-open-nav-icon-white.png"
-          alt=""
+    <>
+      <div className="header-container">
+        <input
+          className="check"
+          checked={isChecked}
+          onChange={toggleCheckbox}
+          type="checkbox"
+          id="check"
         />
-      </label>
-      <ul className="pages-menu">
-        <div className="link-flex">
-          <NavLink
-            to="/project2"
-            className={page === 'home' ? 'normal' : 'normal grey-text'}
-            onClick={() => handlePage('home')}
-          >
-            <h1>Explore</h1>
-          </NavLink>
-        </div>
-        <div className="link-flex">
-          <NavLink
-            to="/project2/filtercars"
-            className={page === 'filter' ? 'normal' : 'normal grey-text'}
-            onClick={() => handlePage('filter')}
-          >
-            <h1>Search</h1>
-          </NavLink>
-        </div>
-        <div className="link-flex">
-          <NavLink
-            to="/project2/favorites"
-            className={page === 'favorites' ? 'normal' : 'normal grey-text'}
-            onClick={() => handlePage('favorites')}
-          >
-            <h1>My Favorites</h1>
-          </NavLink>
-        </div>
-        <div className="link-flex">
-          <NavLink
-            to="/project2/reviewedcars"
-            className={page === 'reviews' ? 'normal' : 'normal grey-text'}
-            onClick={() => handlePage('reviews')}
-          >
-            <h1>My Reviews</h1>
-          </NavLink>
-        </div>
-      </ul>
-    </div>
+        <label className="checkbtn" htmlFor="check">
+          <img
+            className="burger-menu"
+            src="https://www.pngkit.com/png/full/239-2394744_icon-open-nav-icon-white.png"
+            alt=""
+          />
+        </label>
+        <NavLink
+          to="/project2"
+          onClick={() => handlePage('home')}
+        >
+          <img className={page === 'home' ?  'header-logo' : 'header-logo gray-text-logo'} src={logo} alt=""/>
+        </NavLink>        
+        <ul className="pages-menu">
+            <NavLink
+              to="/project2/filtercars"
+              className={page === 'filter' ? 'normal' : 'normal grey-text'}
+              onClick={() => handlePage('filter')}
+            >
+              <h1>Search</h1>
+            </NavLink>
+            <NavLink
+              to="/project2/favorites"
+              className={page === 'favorites' ? 'normal' : 'normal grey-text'}
+              onClick={() => handlePage('favorites')}
+            >
+              <h1>My Favorites</h1>
+            </NavLink>
+            <NavLink
+              to="/project2/reviewedcars"
+              className={page === 'reviews' ? 'normal' : 'normal grey-text'}
+              onClick={() => handlePage('reviews')}
+            >
+              <h1>My Reviews</h1>
+            </NavLink>
+        </ul>
+      </div>
+    </>
   );
 };
 
