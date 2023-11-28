4b6be86947d3d6e9c8fb76f7ef3ba194e419c80e
Filip Skaug <filis@stud.ntnu.no>
2023-11-28 17:39:13 +0100

    #64 create component for infoDropdown

---
+++ frontend/src/components/InfoDropdown.tsx
@@ -0,0 +1,77 @@
+type InfoDropdownProps = {
+  showInfo: boolean;
+  setShowInfo: (showInfo: boolean) => void;
+  formattedPrice: string;
+  carDrivetrain: string;
+  carBody: string;
+  carHorsepower: number;
+  carNumOfDoors: number;
+  carEngineType: string;
+};
+
+function infoDropdown({
+  showInfo,
+  setShowInfo,
+  formattedPrice,
+  carDrivetrain,
+  carBody,
+  carHorsepower,
+  carNumOfDoors,
+  carEngineType,
+  }: InfoDropdownProps) {
+  return (
+    <div className="info-section">
+      <div
+        className="info-wrapper"
+        style={showInfo ? { height: '18rem' } : { height: '0' }}
+      >
+        <div className="info-line"></div>
+        <div className="info">
+          <table>
+            <tbody>
+              <tr>
+                <td>Price</td>
+                <td>{formattedPrice}</td>
+              </tr>
+              <tr>
+                <td>Drivetrain</td>
+                <td>{carDrivetrain}</td>
+              </tr>
+              <tr>
+                <td>Type</td>
+                <td>{carBody}</td>
+              </tr>
+              <tr>
+                <td>Horsepower</td>
+                <td>{carHorsepower}</td>
+              </tr>
+              <tr>
+                <td>Number of doors</td>
+                <td>{carNumOfDoors}</td>
+              </tr>
+              <tr>
+                <td>Type of engine</td>
+                <td>{carEngineType}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div className="info-line">
+        <button
+          className="info-button"
+          onClick={() => setShowInfo(!showInfo)}
+          aria-label="show more info"
+        >
+          <i
+            className={
+              showInfo ? 'arrow-info-button open' : 'arrow-info-button closed'
+            }
+          />
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default infoDropdown;
--- frontend/src/pages/Carpage.tsx
+++ frontend/src/pages/Carpage.tsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom';
 import ReviewSection from '../components/ReviewSection';
 import FavoriteButton from '../components/FavoriteButton';
+import InfoDropdown from '../components/InfoDropdown';
 import '../assets/Carpage.css';
 import { CircularProgress, Rating } from '@mui/material';
 import StarIcon from '@mui/icons-material/Star';
@@ -135,58 +136,16 @@ const Carpage = () => {
             </div>
           </div>
         </div>
-        <div className="info-section">
-          <div
-            className="info-wrapper"
-            style={showInfo ? { height: '18rem' } : { height: '0' }}
-          >
-            <div className="info-line"></div>
-            <div className="info">
-              <table>
-                <tbody>
-                  <tr>
-                    <td>Price</td>
-                    <td>{formattedPrice}</td>
-                  </tr>
-                  <tr>
-                    <td>Drivetrain</td>
-                    <td>{carDrivetrain}</td>
-                  </tr>
-                  <tr>
-                    <td>Type</td>
-                    <td>{carBody}</td>
-                  </tr>
-                  <tr>
-                    <td>Horsepower</td>
-                    <td>{carHorsepower}</td>
-                  </tr>
-                  <tr>
-                    <td>Number of doors</td>
-                    <td>{carNumOfDoors}</td>
-                  </tr>
-                  <tr>
-                    <td>Type of engine</td>
-                    <td>{carEngineType}</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-          <div className="info-line">
-            <button
-              className="info-button"
-              onClick={() => setShowInfo(!showInfo)}
-              aria-label="show more info"
-            >
-              <i
-                className={
-                  showInfo ? 'arrow-info-button open' : 'arrow-info-button closed'
-                }
-              />
-            </button>
-          </div>
-        </div>
-
+        <InfoDropdown
+          showInfo={showInfo}
+          setShowInfo={setShowInfo}
+          formattedPrice={formattedPrice}
+          carDrivetrain={carDrivetrain}
+          carBody={carBody}
+          carHorsepower={carHorsepower}
+          carNumOfDoors={carNumOfDoors}
+          carEngineType={carEngineType}
+        />
         <div>
           <ReviewSection
             userReview={userReviewData.userReviewForCar}
